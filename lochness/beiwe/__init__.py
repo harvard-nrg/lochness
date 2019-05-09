e2ed6c4b11c199a5efe55ad8c0cbe7439fa501c7
Timothy O'Keefe <tokeefe@Timothys-MacBook-Pro.local>
2019-05-09 17:08:47 -0400

    separate GENERAL and PROTECTED

--- lochness/beiwe/__init__.py
+++ lochness/beiwe/__init__.py
@@ -37,27 +37,81 @@ def sync(Lochness, subject, dry=False):
         for uid in beiwe_uids:
             study_frag,beiwe_id = uid
             study_name,study_id = mano.expand_study_id(Keyring, study_frag)
-            logger.info('backfill subject={0}, study={1}, url={2}'.format(beiwe_id, study_name, base_url))
-            dst_beiwe_folder = tree.get('phone', subject.general_folder, beiwe_id=beiwe_id)
+            dst_general_folder = tree.get('phone', subject.general_folder, beiwe_id=beiwe_id)
+            dst_protected_folder = tree.get('phone', subject.protected_folder, beiwe_id=beiwe_id)
             # save a hidden file with the study id, original name, and sanitized name
-            save_study_file(dst_beiwe_folder, study_id, study_name)
-            # do backfill
+            save_study_file(dst_general_folder, study_id, study_name)
+            protected_streams = set(PROTECT)
+            general_streams = set(mano.DATA_STREAMS) - protected_streams
+            # begin backfill download of all general data streams
+            logger.info('backfill general streams for subject={0}, study={1}, url={2}'.format(beiwe_id, study_name, base_url))
+            mano.sync.backfill(
+                Keyring,
+                study_id,
+                beiwe_id,
+                os.path.dirname(dst_general_folder),
+                start_date=str(backfill_start),
+                data_streams=general_streams
+            )
+            # begin backfill download of all protected data streams
             passphrase = Lochness['keyring']['lochness']['SECRETS'].get(subject.study, None)
-            mano.sync.backfill(Keyring, study_id, beiwe_id, os.path.dirname(dst_beiwe_folder), 
-                               str(backfill_start), PROTECT, passphrase)
-            # check for registry file
-            registry_file = os.path.join(dst_beiwe_folder, '.registry')
+            logger.info('backfill protected streams for subject={0}, study={1}, url={2}'.format(beiwe_id, study_name, base_url))
+            mano.sync.backfill(
+                Keyring,
+                study_id,
+                beiwe_id,
+                os.path.dirname(dst_protected_folder),
+                start_date=str(backfill_start),
+                data_streams=protected_streams,
+                lock=protected_streams,
+                passphrase=passphrase
+            )
+            # begin delta download of all general data streams
+            registry_file = os.path.join(dst_general_folder, '.registry')
+            logger.debug('reading in registry file {0}'.format(registry_file))
+            if not os.path.exists(registry_file):
+                logger.error('could not find registry file {0}'.format(registry_file))
+                continue
+            with open(registry_file) as fo:
+                registry = fo.read()
+            logger.info('delta download of general data streams for subject={0}, study={1}, url={2}'.format(beiwe_id, study_name, base_url))
+            archive = mano.sync.download(
+                Keyring,
+                study_id,
+                beiwe_id,
+                data_streams=general_streams,
+                registry=registry
+            )
+            mano.sync.save(
+                Keyring,
+                archive,
+                beiwe_id,
+                os.path.dirname(dst_general_folder)
+            )
+            # begin delta download of all protected streams
+            registry_file = os.path.join(dst_protected_folder, '.registry')
             logger.debug('reading registry file {0}'.format(registry_file))
             if not os.path.exists(registry_file):
                 logger.error('could not find registry file {0}'.format(registry_file))
                 continue
-            # read registry file
             with open(registry_file) as fo:
                 registry = fo.read()
-            # sync up to today
-            logger.info('delta subject={0}, study={1}, url={2}'.format(beiwe_id, study_name, base_url))
-            archive = mano.sync.download(Keyring, study_id, beiwe_id, registry=registry)
-            mano.sync.save(Keyring, archive, beiwe_id, os.path.dirname(dst_beiwe_folder), PROTECT, passphrase)
+            logger.info('delta download of protected data streams for subject={0}, study={1}, url={2}'.format(beiwe_id, study_name, base_url))
+            archive = mano.sync.download(
+                Keyring,
+                study_id,
+                beiwe_id,
+                data_streams=protected_streams,
+                registry=registry
+            )
+            mano.sync.save(
+                Keyring,
+                archive,
+                beiwe_id,
+                os.path.dirname(dst_protected_folder),
+                lock=protected_streams,
+                passphrase=passphrase
+            )
 
 def save_study_file(d, study_id, study_name):
     study_file = os.path.join(d, '.study')
