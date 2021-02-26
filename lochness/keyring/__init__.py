349fcd9ef35484589499c3c0e5e90257e54c04f2
kevin cho <sky8671@gmail.com>
2021-02-26 14:42:27 -0500

    feat: box_api_token function added

--- lochness/keyring/__init__.py
+++ lochness/keyring/__init__.py
@@ -12,6 +12,14 @@ def dropbox_api_token(Lochness, key):
         raise KeyringError('\'API_TOKEN\' not in {0}'.format(key))
     return Lochness['keyring'][key]['API_TOKEN']
 
+def box_api_token(Lochness, key):
+    '''get box api token from keyring'''
+    if key not in Lochness['keyring']:
+        raise KeyringError('\'{0}\' not in keyring'.format(key))
+    if 'API_TOKEN' not in Lochness['keyring'][key]:
+        raise KeyringError('\'API_TOKEN\' not in {0}'.format(key))
+    return Lochness['keyring'][key]['API_TOKEN']
+
 class KeyringError(Exception):
     pass
 
