b70b70b6dd943a87fb637f1f231e9f59f461e0fd
kevin cho <sky8671@gmail.com>
2021-03-02 18:42:45 -0500

    feat: box_api_token function added

--- lochness/keyring/__init__.py
+++ lochness/keyring/__init__.py
@@ -1,5 +1,6 @@
 def passphrase(Lochness, study):
     '''get passphrase for study from keyring'''
+
     if study not in Lochness['keyring']['lochness']['SECRETS']:
         raise KeyringError('lochness.SECRETS.{0} not found'.format(study))
     return Lochness['keyring']['lochness']['SECRETS'][study]
@@ -18,7 +19,10 @@ def box_api_token(Lochness, key):
         raise KeyringError('\'{0}\' not in keyring'.format(key))
     if 'API_TOKEN' not in Lochness['keyring'][key]:
         raise KeyringError('\'API_TOKEN\' not in {0}'.format(key))
-    return Lochness['keyring'][key]['API_TOKEN']
+    return (Lochness['keyring'][key]['CLIENT_ID'],
+            Lochness['keyring'][key]['CLIENT_SECRET'],
+            Lochness['keyring'][key]['API_TOKEN'])
+
 
 class KeyringError(Exception):
     pass
