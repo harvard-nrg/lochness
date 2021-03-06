10f3240c8e25cc48492ac08e47f36292283a1dfe
kevin cho <sky8671@gmail.com>
2021-03-06 08:20:34 -0500

    fix: tree.get to return either 'raw_folder' or 'processed_folder' based on kwargs processed

--- lochness/tree/__init__.py
+++ lochness/tree/__init__.py
@@ -62,28 +62,7 @@ Templates = {
 
 logger = logging.getLogger(__name__)
 
-def get(type, base, **kwargs):
-    '''get phoenix folder for a subject and datatype'''
-    if type not in Templates:
-        raise TreeError('no tree templates defined for {0}'.format(type))
-    raw_folder = None
-    processed_folder = None
-    if 'raw' in Templates[type]:
-        raw_folder = Templates[type]['raw'].substitute(base=base, **kwargs)
-    if 'processed' in Templates[type]:
-        processed_folder = Templates[type]['processed'].substitute(base=base, **kwargs)
-    if kwargs.get('makedirs', True):
-        if raw_folder and not os.path.exists(raw_folder):
-            logger.debug('creating raw folder {0}'.format(raw_folder))
-            os.makedirs(raw_folder)
-        if processed_folder and not os.path.exists(processed_folder):
-            logger.debug('creating processed folder {0}'.format(processed_folder))
-            os.makedirs(processed_folder)
-            os.chmod(processed_folder, 0o01777)
-    return raw_folder
-
-
-def return_local_path(data_type, base, **kwargs):
+def get(data_type, base, **kwargs):
     '''get phoenix folder for a subject and data_type'''
     if data_type not in Templates:
         raise TreeError('no tree templates defined for {0}'.format(data_type))
