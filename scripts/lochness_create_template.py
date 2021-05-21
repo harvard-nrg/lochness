aeae912a3a96b9cf252442346df6f6e4525dd9cd
Kang Ik Kevin Cho <kevincho@bwh.harvard.edu>
2021-05-21 13:21:53 -0400

    minor: comment fix

--- scripts/lochness_create_template.py
+++ scripts/lochness_create_template.py
@@ -68,7 +68,7 @@ def create_lochness_template(args):
             })
         df.to_csv(args.pii_csv)
 
-    # create pii table
+    # link lochness_sycn_history timestamp db
     if not Path(args.lochness_sync_history_csv).is_file():
         args.lochness_sync_history_csv = args.outdir / \
                 'lochness_sync_history.csv'
