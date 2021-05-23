6be2ff8458fe387d1cb6150479d7845a1eb2dfeb
Kang Ik Kevin Cho <kevincho@bwh.harvard.edu>
2021-05-23 10:54:03 -0400

    feat: daris test to uuse test_lochness module

--- tests/lochness_test/daris/test_daris.py
+++ tests/lochness_test/daris/test_daris.py
@@ -4,34 +4,34 @@ import lochness.config as config
 
 from pathlib import Path
 import sys
-scripts_dir = Path(lochness.__path__[0]).parent / 'scripts'
-sys.path.append(str(scripts_dir))
 
-from lochness_create_template import create_lochness_template
 import pytest
 import shutil
 import os
 
-scripts_dir = Path(lochness.__path__[0]).parent / 'tests'
-sys.path.append(str(scripts_dir))
 import zipfile
 import tempfile as tf
 import json
 import cryptease as crypt
 import pandas as pd
 
+lochness_root = Path(lochness.__path__[0]).parent
+scripts_dir = lochness_root / 'scripts'
+test_dir = lochness_root / 'tests'
+sys.path.append(str(scripts_dir))
+sys.path.append(str(test_dir))
+from lochness_create_template import create_lochness_template
 
-class Args:
+from test_lochness import Args, Tokens, KeyringAndEncrypt, args, Lochness
+from test_lochness import show_tree_then_delete, config_load_test
+
+
+class Args(Args):
     def __init__(self, root_dir):
-        self.outdir = root_dir
+        super().__init__(root_dir)
         self.studies = ['StudyA']
         self.sources = ['Redcap', 'xNat', 'boX', 'daris']
         self.poll_interval = 10
-        self.ssh_user = 'kc244'
-        self.ssh_host = 'erisone.partners.org'
-        self.email = 'kevincho@bwh.harvard.edu'
-        self.det_csv = 'prac.csv'
-        self.pii_csv = ''
 
 
 @pytest.fixture
@@ -39,10 +39,51 @@ def args():
     return Args('test_lochness')
 
 
+class KeyringAndEncryptDaris(KeyringAndEncrypt):
+    def __init__(self, tmp_dir):
+        super().__init__(tmp_dir)
+        token = TokensDaris()
+        token, url, project_cid = token.get_token_daris()
+
+        self.keyring['daris.StudyA']['TOKEN'] = token
+        self.keyring['daris.StudyA']['URL'] = url
+        self.keyring['daris.StudyA']['PROJECT_CID'] = project_cid
+
+        self.write_keyring_and_encrypt()
+
+
+class TokensDaris(Tokens):
+    def __init__(self):
+        super().__init__()
+
+    def get_token_daris(self):
+        print(self.token_and_url_file)
+        if self.token_and_url_file.is_file():
+            df = pd.read_csv(self.token_and_url_file)
+            token = df.iloc[0]['token']
+            url = df.iloc[0]['url']
+            project_cid = df.iloc[0]['project_id']
+        else:
+            token = input('Enter token: ')
+            url = input('Enter URL: ')
+            project_cid = input('Enter project CID: ')
+
+        return token, url, project_cid
+
+@pytest.fixture
+def Lochness():
+    args = Args('tmp_lochness')
+    create_lochness_template(args)
+    KeyringAndEncryptDaris(args.outdir)
+
+    lochness = config_load_test('tmp_lochness/config.yml', '')
+    return lochness
+
 def test_daris_download():
     daris_uid = 'subject01'
     latest_pull_mtime = 0
-    token, url, project_cid = get_token_url_project_cid()
+    token = TokensDaris()
+    token, url, project_cid = token.get_token_daris()
     dst_zipfile = 'tmp.zip'
 
     daris_download(daris_uid, latest_pull_mtime, token,
@@ -71,26 +112,6 @@ def test_daris_download():
 
 
 
-def update_keyring_and_encrypt(tmp_lochness_dir: str):
-    token, url, project_cid = get_token_url_project_cid()
-    keyring_loc = Path(tmp_lochness_dir) / 'lochness.json'
-    with open(keyring_loc, 'r') as f:
-        keyring = json.load(f)
-
-    keyring['daris.StudyA']['TOKEN'] = token
-    keyring['daris.StudyA']['URL'] = url
-    keyring['daris.StudyA']['PROJECT_CID'] = project_cid
-
-
-    with open(keyring_loc, 'w') as f:
-        json.dump(keyring, f)
-    
-    keyring_content = open(keyring_loc, 'rb')
-    key = crypt.kdf('')
-    crypt.encrypt(keyring_content, key,
-            filename=Path(tmp_lochness_dir) / '.lochness.enc')
-
-
 def initialize_metadata(Lochness, study_name):
     df = pd.DataFrame({'Active': [1],
         'Consent': '1988.-09-16',
@@ -102,35 +123,19 @@ def initialize_metadata(Lochness, study_name):
     df.to_csv(df_loc, index=False)
 
 
-def get_token_url_project_cid():
-    token_and_url_file = Path('token_and_url.txt')
-
-    if token_and_url_file.is_file():
-        df = pd.read_csv(token_and_url_file)
-        token = df.iloc[0]['token']
-        url = df.iloc[0]['url']
-        project_cid = df.iloc[0]['project_id']
-    else:
-        token = input('Enter token: ')
-        url = input('Enter URL: ')
-        project_cid = input('Enter project CID: ')
-
-    return token, url, project_cid
-
-
-def test_sync_from_empty(args):
-    outdir = 'tmp_lochness'
-    args.outdir = outdir
-    create_lochness_template(args)
-    update_keyring_and_encrypt(args.outdir)
+def test_sync_from_empty(Lochness):
+    # outdir = 'tmp_lochness'
+    # args.outdir = outdir
+    # create_lochness_template(args)
+    # update_keyring_and_encrypt(args.outdir)
     dry=False
     study_name = 'StudyA'
-    Lochness = config.load(f'{args.outdir}/config.yml', '')
     initialize_metadata(Lochness, study_name)
 
     for subject in lochness.read_phoenix_metadata(Lochness,
                                                   studies=[study_name]):
         sync(Lochness, subject, dry)
-    shutil.rmtree(args.outdir)
+
+    show_tree_then_delete('tmp_lochness')
 
 
