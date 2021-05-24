32603cc73d4d349b289a3390eebcaa934613e42b
Kang Ik Kevin Cho <kevincho@bwh.harvard.edu>
2021-05-24 12:23:35 -0400

    feat: testing at the top abstraction level

--- tests/scripts_test/test_sync.py
+++ tests/scripts_test/test_sync.py
@@ -1,34 +1,50 @@
 import lochness
 from pathlib import Path
 import sys
-scripts_dir = Path(lochness.__path__[0]).parent / 'scripts'
-sys.path.append(str(scripts_dir))
 
-import sync
+
+import sys
+lochness_root = Path(lochness.__path__[0]).parent
+scripts_dir = lochness_root / 'scripts'
+test_dir = lochness_root / 'tests'
+sys.path.append(str(scripts_dir))
+sys.path.append(str(test_dir))
+
+from test_lochness import Tokens, KeyringAndEncrypt, args, SyncArgs
+from test_lochness import show_tree_then_delete, config_load_test
+
+from lochness_create_template import create_lochness_template
+
+
 import pytest
 import lochness.config as config
+from sync import do
+
+from lochness_test.transfer.test_transfer import KeyringAndEncryptLochnessTransfer
 
 
-class Args(object):
-    def __init__(self):
-        self.source = ['xnat', 'box', 'redcap']
-        self.config = '/Users/kevin/Dropbox/PNL/Projects/' \
-                '2021_lochness/suggest_config.yml'
-        self.archive_base = None
-    def __str__(self):
-        return 'haha'
 
-@pytest.fixture
-def simple_args():
-    arg = Args()
-    return arg
+def test_do_init(args):
+    syncArgs = SyncArgs()
+    create_lochness_template(args)
+    syncArgs.config = args.outdir / 'config.yml'
+    _ = KeyringAndEncrypt(args.outdir)
+    Lochness = config_load_test(syncArgs.config, syncArgs.archive_base)
 
-def test_do():
-    args = Args()
-    Lochness = config.load(args.config, args.archive_base)
+
+def test_do_with_lochness_sync_receive(args):
+    syncArgs = SyncArgs()
+    syncArgs.lochness_sync_receive = True
+    create_lochness_template(args)
+    syncArgs.config = args.outdir / 'config.yml'
+    syncArgs.sources = args.sources
+    _ = KeyringAndEncryptLochnessTransfer(args.outdir)
+
+    do(syncArgs)
+
 
 def test_read_phoenix_data():
-    args = Args()
+    args = SyncArgs()
     args.source = ['xnat', 'box']
     args.studies = ['mclean']
     args.dry = [False]
