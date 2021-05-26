3638ee084e079dd482708b93f3ba719be4c5809b
Kang Ik Kevin Cho <kevincho@bwh.harvard.edu>
2021-05-26 14:47:43 -0400

    feat: merged other redcap function tests into this script

--- tests/lochness_test/redcap/test_redcap.py
+++ tests/lochness_test/redcap/test_redcap.py
@@ -6,6 +6,7 @@ from pathlib import Path
 
 from lochness.redcap import sync, initialize_metadata
 from lochness.redcap.data_trigger_capture import save_post_from_redcap
+from lochness.redcap.data_trigger_capture import back_up_db
 
 import sys
 lochness_root = Path(lochness.__path__[0]).parent
@@ -207,4 +208,24 @@ def test_sync_det_update_while_diff_file_leads_to_data_overwrite(
     assert {'test':'test'} != new_content_dict
     rmtree('tmp_lochness')
 
+
+def test_save_post_from_redcap():
+    text_body = "redcap_url=https%3A%2F%2Fredcap.partners.org%2Fredcap%2F&project_url=https%3A%2F%2Fredcap.partners.org%2Fredcap%2Fredcap_v10.0.30%2Findex.php%3Fpid%3D26709&project_id=26709&username=kc244&record=1001_1&instrument=inclusionexclusion_checklist&inclusionexclusion_checklist_complete=0"
+    save_post_from_redcap(text_body, 'ha.csv')
+
+
+def test_save_post_from_redcap_no_redcap_post():
+    text_body = "hahahoho"
+    if 'redcap' in text_body:
+        save_post_from_redcap(text_body, 'ha.csv')
+    else:
+        print('no redcap post')
+
+
+def test_back_up_db():
+    back_up_db('ha.csv')
+    text_body = "redcap_url=https%3A%2F%2Fredcap.partners.org%2Fredcap%2F&project_url=https%3A%2F%2Fredcap.partners.org%2Fredcap%2Fredcap_v10.0.30%2Findex.php%3Fpid%3D26709&project_id=26709&username=kc244&record=1001_1&instrument=inclusionexclusion_checklist&inclusionexclusion_checklist_complete=0"
+    save_post_from_redcap(text_body, 'ha.csv')
+
+    back_up_db('ha.csv')
 
