76e239a3cecfb0e798da0cc640a5b29b7842a3c9
Kang Ik Kevin Cho <kevincho@bwh.harvard.edu>
2021-05-27 14:53:30 -0400

    feat: module test added to the shell script

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -1,7 +1,9 @@
+pytest lochness_test/xnat/test_xnat.py
+pytest lochness_test/box/test_box.py
 pytest lochness_test/daris/test_daris.py
 pytest lochness_test/redcap/test_redcap.py
 pytest lochness_test/keyring/test_keyring.py
 pytest lochness_test/mindlamp/test_mindlamp.py
+
 pytest scripts_test/test_sync.py
 pytest scripts_test/test_lochness_create_template.py
-
