fea1001c681f9a097e09eec3155bd5dba2c3ed56
Kang Ik Kevin Cho <kevincho@bwh.harvard.edu>
2021-06-14 12:37:00 -0400

    fix: test without TOKEN - working version

--- tests/lochness_test/mediaflux/test_mediaflux.py
+++ tests/lochness_test/mediaflux/test_mediaflux.py
@@ -29,7 +29,7 @@ from test_lochness import show_tree_then_delete, config_load_test
 class Args(Args):
     def __init__(self, root_dir):
         super().__init__(root_dir)
-        self.studies = ['StudyA']
+        self.studies = ['PrescientAD']
         self.sources = ['Redcap', 'xNat', 'boX', 'mediaflux']
         self.poll_interval = 10
 
@@ -40,55 +40,63 @@ def args():
 
 
 class KeyringAndEncryptMediaflux(KeyringAndEncrypt):
-    def __init__(self, tmp_dir):
+    def __init__(self, tmp_dir, study):
         super().__init__(tmp_dir)
         token = Tokens()
         HOST, PORT, TRANSPORT, TOKEN, DOMAIN, USER, PASSWORD = \
                 token.read_token_or_get_input('mediaflux')
 
-        self.keyring['mediaflux.StudyA'] = {}
-        self.keyring['mediaflux.StudyA']['HOST'] = HOST
-        self.keyring['mediaflux.StudyA']['PORTJ'] = PORT
-        self.keyring['mediaflux.StudyA']['TRANSPORT'] = TRANSPORT
-        self.keyring['mediaflux.StudyA']['TOKEN'] = TOKEN
-        self.keyring['mediaflux.StudyA']['DOMAIN'] = DOMAIN
-        self.keyring['mediaflux.StudyA']['USER'] = USER
-        self.keyring['mediaflux.StudyA']['PASSWORD'] = PASSWORD
+        self.keyring[f'mediaflux.{study}'] = {}
+        self.keyring[f'mediaflux.{study}']['HOST'] = HOST
+        self.keyring[f'mediaflux.{study}']['PORT'] = PORT
+        self.keyring[f'mediaflux.{study}']['TRANSPORT'] = TRANSPORT
+        # self.keyring['mediaflux.StudyA']['TOKEN'] = TOKEN
+        self.keyring[f'mediaflux.{study}']['DOMAIN'] = DOMAIN
+        self.keyring[f'mediaflux.{study}']['USER'] = USER
+        self.keyring[f'mediaflux.{study}']['PASSWORD'] = PASSWORD
 
         self.write_keyring_and_encrypt()
 
 
-
 @pytest.fixture
-def Lochness():
+def args_and_Lochness():
     args = Args('tmp_lochness')
     create_lochness_template(args)
-    KeyringAndEncryptMediaflux(args.outdir)
+    for study in args.studies:
+        KeyringAndEncryptMediaflux(args.outdir, study)
 
     lochness = config_load_test('tmp_lochness/config.yml', '')
-    return lochness
+
+    return args, lochness
 
 
 def initialize_metadata(Lochness, study_name):
-    df = pd.DataFrame({'Active': [1],
-        'Consent': '1988.-09-16',
-        'Subject ID': 'subject01',
-        'Mediaflux': 'mediaflux.StudyA:5Yp0E'})
+    df = pd.DataFrame({'Active': [1, 1, 1],
+        'Consent': ['1988-09-16', '1988-09-16', '1988-09-16'],
+        'Subject ID': ['subject01', 'subject02', 'subject03'],
+        'Mediaflux': [f'mediaflux.{study_name}:AD123453',
+                      f'mediaflux.{study_name}:AD124352',
+                      f'mediaflux.{study_name}:AD124539']
+        })
+
     df_loc = Path(Lochness['phoenix_root']) / 'GENERAL' / \
             study_name / f"{study_name}_metadata.csv"
 
     df.to_csv(df_loc, index=False)
 
 
-def test_sync_from_empty(Lochness):
+def test_sync_from_empty(args_and_Lochness):
+    args, Lochness = args_and_Lochness
     dry=False
-    study_name = 'StudyA'
-    initialize_metadata(Lochness, study_name)
+    for study_name in args.studies:
+        initialize_metadata(Lochness, study_name)
+        Lochness['mediaflux'][study_name]['namespace'] = \
+            '/projects/proj-5070_prescient-1128.4.380' \
+            '/example_mediaflux_root/' + study_name
 
-    for subject in lochness.read_phoenix_metadata(Lochness,
-                                                  studies=[study_name]):
-        sync(Lochness, subject, dry)
+        for subject in lochness.read_phoenix_metadata(Lochness,
+                                                      studies=[study_name]):
+            sync(Lochness, subject, dry)
 
     show_tree_then_delete('tmp_lochness')
-
 
